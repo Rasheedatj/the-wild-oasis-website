04edb3dd217e4e03e92e08c96589d8ed2a21ad8d
Rasheedat Jinadu <jinaduikeoluwa@gmail.com>
2025-01-16 16:38:35 +0100

    feat: implement loading state for cabins

---
+++ app/cabins/loading.js
@@ -0,0 +1,12 @@
+import SpinnerMini from '@/app/_components/SpinnerMini';
+
+function Loading() {
+  return (
+    <div className='grid place-items-center text-primary-200'>
+      <SpinnerMini />
+      <p className='text-lg'>Loading cabin, please wait...</p>
+    </div>
+  );
+}
+
+export default Loading;
--- app/loading.js
+++ app/loading.js
@@ -1,9 +1,7 @@
+import Spinner from '@/app/_components/Spinner';
+
 function Loading() {
-  return (
-    <div>
-      <p>Loading data...</p>
-    </div>
-  );
+  return <Spinner />;
 }
 
 export default Loading;
