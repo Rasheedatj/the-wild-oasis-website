52cbaf628ff73559dc53baa11ed72953e7872c5b
Rasheedat Jinadu <jinaduikeoluwa@gmail.com>
2025-01-30 08:38:31 +0100

    feat: implement revalidate api on cabins and about

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -1,5 +1,6 @@
 import { eachDayOfInterval } from 'date-fns';
 import { supabase } from './supabase';
+import { notFound } from 'next/navigation';
 
 export async function getCabin(id) {
   const { data, error } = await supabase
@@ -12,6 +13,7 @@ export async function getCabin(id) {
   // await new Promise((res) => setTimeout(res, 1000));
 
   if (error) {
+    notFound();
     console.error(error);
   }
 
