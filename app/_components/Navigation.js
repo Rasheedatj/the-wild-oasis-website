bf299c3e7eb58758454b754b12b24212c68095cc
Rasheedat Jinadu <jinaduikeoluwa@gmail.com>
2025-01-30 15:14:25 +0100

    feat:position faq images

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,33 +1,42 @@
+'use client';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const links = [
+  {
+    id: 1,
+    href: '/cabins',
+    label: 'Cabins',
+  },
+  {
+    id: 2,
+    href: '/about',
+    label: 'About',
+  },
+  {
+    id: 3,
+    href: '/account',
+    label: 'Guest area',
+  },
+];
 
 export default function Navigation() {
+  const pathName = usePathname();
   return (
     <nav className='z-10 text-xl'>
       <ul className='flex gap-16 items-center'>
-        <li>
-          <Link
-            href='/cabins'
-            className='hover:text-accent-400 transition-colors'
-          >
-            Cabins
-          </Link>
-        </li>
-        <li>
-          <Link
-            href='/about'
-            className='hover:text-accent-400 transition-colors'
-          >
-            About
-          </Link>
-        </li>
-        <li>
-          <Link
-            href='/account'
-            className='hover:text-accent-400 transition-colors'
-          >
-            Guest area
-          </Link>
-        </li>
+        {links.map((link) => (
+          <li key={link.id}>
+            <Link
+              href={link.href}
+              className={`hover:text-accent-400 py-3 px-5 transition-colors ${
+                pathName === link.href && 'bg-primary-900'
+              }`}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
